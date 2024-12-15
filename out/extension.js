b9fa2fa1bd2cc855ff376cccb6e84ec4d562c174
Rebecca <lrk145@seas.upenn.edu>
2024-12-15 12:59:13 -0700

    update again

--- out/extension.js
+++ out/extension.js
@@ -35,8 +35,6 @@ var __importStar = (this && this.__importStar) || (function () {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = activate;
 exports.deactivate = deactivate;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(require("vscode"));
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -44,15 +42,131 @@ function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "devpace" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
     const disposable = vscode.commands.registerCommand('devpace.helloWorld', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World from devpace!');
     });
     context.subscriptions.push(disposable);
+    let startupmenu = vscode.commands.registerCommand('devpace.start', () => {
+        //Create and show the startup window
+        const panel = vscode.window.createWebviewPanel('setupWindow', 'Setup', vscode.ViewColumn.One, {});
+        panel.webview.html = getWebViewContent();
+    });
+    context.subscriptions.push(startupmenu);
+    let testphrase = 'teststring';
+    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(() => messagePopUps(testphrase)));
+}
+function getWebViewContent() {
+    return `<!DOCTYPE html>
+	<html>
+	<head>
+		<style>
+			img {
+				height: 100px;
+				width: 100px;
+			}
+			h1 {
+				color: green;
+				}
+
+	</style>
+	</head>
+	<body>
+
+	<h1> Inital setup </h1>
+	<table>
+		<tr>
+			<td>
+			<label for ="activity">Activity:</label>
+			</td>
+			<td>
+			<label for ="startTime">Start Time:</label>
+			</td>
+			<td>
+			<label for ="stopTime">Stop Time:</label>
+			</td>
+			<td>
+			<label for ="interval">Interval:</label>
+			</td>
+		<tr>
+			<td>
+			<img src="/waterimage.jpg">
+			</td>
+			<td>
+			<input type="text" id="fname" name="fname">
+			</td>
+			<td>
+			<input type="text" id="fname" name="fname">
+			</td>
+			<td>
+			<input type="text" id="fname" name="fname">
+			</td>
+		</tr>
+		<tr>
+			<td>
+			<label for ="walk">walk:</label>
+			</td>
+			<td>
+			<input type="number" id="reminderInterval" name="reminder">
+			</td>
+			<td>
+			<input type="text" id="fname" name="fname">
+			</td>
+			<td>
+			<input type="text" id="fname" name="fname">
+			</td>
+		</tr>
+		<tr>
+			<td>
+			<label for="Drink">Drinks:</label>
+			</td>
+			<td>
+			<input type="text" id="fname" name="fname">
+			</td>
+			<td>
+			<input type="text" id="fname" name="fname">
+			</td>
+			<td>
+			<input type="text" id="fname" name="fname">
+			</td>
+		</tr>
+	</table>
+	<br><br>
+	<button onclick="saveSettings()">Submit</button>
+	</body>
+	</html>
+	`;
+}
+const phrase = vscode.window.createTextEditorDecorationType({
+    after: {
+        margin: "0 0 0 3em",
+        textDecoration: "none",
+    },
+    isWholeLine: true,
+    rangeBehavior: vscode.DecorationRangeBehavior.ClosedOpen,
+});
+function messagePopUps(text) {
+    if (vscode.window.activeTextEditor) {
+        const editor = vscode.window.activeTextEditor;
+        const decorationOptions = {
+            range: new vscode.Range(editor.selection.start, editor.selection.end),
+            renderOptions: {
+                after: {
+                    contentText: text,
+                    fontWeight: "normal",
+                    fontStyle: "normal",
+                },
+            },
+        };
+        editor.setDecorations(phrase, [decorationOptions]);
+        setHideTimeout();
+    }
+}
+function setHideTimeout() {
+    setTimeout(() => {
+        vscode.window.activeTextEditor.setDecorations(phrase, []);
+    }, 3500);
 }
 // This method is called when your extension is deactivated
 function deactivate() { }
